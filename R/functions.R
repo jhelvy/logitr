5c38a4929e3387aa1758a201a3f08108842f2444
John Paul Helveston <john.helveston@gmail.com>
2018-02-27 08:59:34 -0500

    Updated default arguments for main logitr function

--- R/functions.R
+++ R/functions.R
@@ -16,23 +16,9 @@
 #' @export
 #' @examples
 #' # Put example code here
-logitr = function(data, choiceName, obsIDName, betaNames, betaDist=NULL,
-                  priceName=NULL, priceDist=NULL, prefSpaceModel=NULL,
-                  standardDraws=NULL,
-                  options = list(
-                    wtpSpace        = F,
-                    numMultiStarts  = 1,
-                    keepAllRuns     = F,
-                    useAnalyticGrad = T,
-                    scaleInputs     = F,
-                    numDraws        = 200,
-                    drawType        = 'halton',
-                    printLevel      = 0,
-                    xtol_rel        = 1.0e-9,
-                    xtol_abs        = 1.0e-9,
-                    ftol_rel        = 1.0e-9,
-                    ftol_abs        = 1.0e-9,
-                    maxeval         = 1000)) {
+logitr = function(data, choiceName, obsIDName, betaNames, priceName=NULL,
+                  betaDist=NULL, priceDist=NULL, prefSpaceModel=NULL,
+                  standardDraws=NULL, options=list()) {
     require(data.table)  # Required for fast computation in the
                          # function getMnlLogit()
     require(randtoolbox) # Required for taking Halton draws
@@ -58,12 +44,12 @@ runOptionsChecks = function(options) {
     # Run checks for all inputs
     if(is.null(options$wtpSpace))        {options$wtpSpace        = F}
     if(is.null(options$numMultiStarts))  {options$numMultiStarts  = 1}
+    if(options$numMultiStarts < 1)       {options$numMultiStarts  = 1}
     if(is.null(options$keepAllRuns))     {options$keepAllRuns     = F}
     if(is.null(options$useAnalyticGrad)) {options$useAnalyticGrad = T}
     if(is.null(options$scaleInputs))     {options$scaleInputs     = F}
     if(is.null(options$numDraws))        {options$numDraws        = 200}
     if(is.null(options$drawType))        {options$drawType        = 'halton'}
-    if(options$numMultiStarts < 1)       {options$numMultiStarts  = 1}
     if(is.null(options$printLevel))      {options$printLevel      = 0}
     if(is.null(options$xtol_rel))        {options$xtol_rel        = 1.0e-9}
     if(is.null(options$xtol_abs))        {options$xtol_abs        = 1.0e-9}
