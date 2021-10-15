b7db9f719edd12f26dec07d9d4ccd504bb232a0f
John Helveston <john.helveston@gmail.com>
2021-10-15 13:10:26 -0400

    working on predict example for vignette

--- inst/example/predict.R
+++ inst/example/predict.R
@@ -16,59 +16,63 @@ mxl_wtp  <- readRDS(here::here('inst', 'extdata', 'mxl_wtp.Rds'))
 # You can make predictions for any set of alternatives, such as hold out
 # samples or within-sample. By default, if no new data are provided via the
 # newdata argument, then predictions will be made for the original data.
-predictions <- predict(mnl_pref)
-head(predictions)
 
-# If you only want the predictions to be returned without the data, set
-# returnData = FALSE
-predictions <- predict(mnl_pref, returnData = FALSE)
-head(predictions)
+# By default, the predict function returns probabilities:
+probs <- predict(mnl_pref)
+head(probs)
+
+# If you want the original data to also be returned in the data frame, set
+# returnData = TRUE
+probs <- predict(mnl_pref, returnData = TRUE)
+head(probs)
 
 # To make predictions for a new set of alternatives, use the newdata argument.
 # In this example, I just use two of the choice observations from the
 # yogurt dataset:
-alts <- subset(
+data <- subset(
   yogurt, obsID %in% c(42, 13),
   select = c('obsID', 'alt', 'price', 'feat', 'brand'))
 
-alts
+data
 
-# Compute choice probabilities for the alts data frame using the preference
-# space MNL model:
+# Compute choice probabilities using the preference space MNL model:
 probs_mnl_pref <- predict(
   mnl_pref,
-  newdata  = alts,
+  newdata = data,
+  obsID = "obsID",
+)
+
+probs_mnl_pref
+
+# You can also get a confidence interval of predicted probabilities by including
+# the `ci` argument. For example, a 95% CI is obtained with `ci = 0.95`:
+probs_mnl_pref <- predict(
+  mnl_pref,
+  newdata = data,
   obsID = "obsID",
   ci = 0.95
 )
 
 probs_mnl_pref
 
-# The results show the expected choice probabilities for each alternative.
-# The low and high values show a 95% confidence interval, estimated using
-# simulation. You can change the CI level by setting ci to a different
-# value (e.g. a 90% CI is obtained with ci = 0.90).
-
-# Compute choice probabilities using the WTP space MNL model:
+# Probabilities can also be computed using WTP space models, but you have to
+# specify the column in "newdata" that corresponds to "price":
 probs_mnl_wtp <- predict(
   mnl_wtp,
-  newdata = alts,
+  newdata = data,
   obsID = "obsID",
+  price = "price",
   ci = 0.95
 )
 
 probs_mnl_wtp
 
-# Since these two models are equivalent except in different spaces, the
-# simulation results should be the same. Note that 'price' is the name
-# of the price attribute in the alts argument and must be included for
-# WTP space models.
-
 # Simulations can also be run using MXL models in either space:
 probs_mxl_pref <- predict(
   mxl_pref,
-  newdata = alts,
-  obsID = "obsID"
+  newdata = data,
+  obsID = "obsID",
+  ci = 0.95
 )
 
 probs_mxl_pref
@@ -76,16 +80,12 @@ probs_mxl_pref
 probs_mxl_wtp <- predict(
   mxl_wtp,
   newdata = alts,
-  obsID = "obsID"
+  obsID = "obsID",
+  price = "price",
+  ci = 0.95
 )
 
 probs_mxl_wtp
-
-# Save results
-saveRDS(probs_mnl_pref, here::here('inst', 'extdata', 'probs_mnl_pref.Rds'))
-saveRDS(probs_mnl_wtp,  here::here('inst', 'extdata', 'probs_mnl_wtp.Rds'))
-saveRDS(probs_mxl_pref, here::here('inst', 'extdata', 'probs_mxl_pref.Rds'))
-saveRDS(probs_mxl_wtp,  here::here('inst', 'extdata', 'probs_mxl_wtp.Rds'))
 
 # Plot simulation results from each model:
 library(ggplot2)
@@ -100,9 +100,9 @@ probs <- rbind(
     obs = paste0("Observation ID: ", obsID)
   )
 
-probs <- ggplot(probs, aes(x = alt, y = prob_mean, fill = model)) +
+probs <- ggplot(probs, aes(x = alt, y = prob_predict, fill = model)) +
     geom_bar(stat = 'identity', width = 0.7, position = "dodge") +
-    geom_errorbar(aes(ymin = prob_low, ymax = prob_high),
+    geom_errorbar(aes(ymin = prob_predict_lower, ymax = prob_predict_upper),
                   width = 0.2, position = position_dodge(width = 0.7)) +
     facet_wrap(vars(obs)) +
     scale_y_continuous(limits = c(0, 1)) +
@@ -115,36 +115,27 @@ ggsave(here::here('vignettes', 'probs.png'),
 
 
 
-
 # You can also predict choices using the type argument. For this example, I
 # will predict choices for every alternative in the data used to estimate the
 # model using the preference space MNL model:
 choices_mnl_pref <- predict(mnl_pref, type = "choices")
 head(choices_mnl_pref)
 
-# You can also make predictions using WTP space models:
+# You can also make predictions using WTP space models as well as MXL models
+# in either space
 choices_mnl_wtp <- predict(mnl_wtp, type = "choices")
 head(choices_mnl_wtp)
 
-# Since these two models are equivalent except in different spaces, the
-# simulation results should be approximately the same.
-
-# Simulations can also be run using MXL models in either space:
 choices_mxl_pref <- predict(mxl_pref, type = "choices")
 head(choices_mxl_pref)
 
-choices_mxl_wtp <- predict(mxl_wtp, type = "choices")
+choices_mxl_wtp <- predict(mxl_wtp, type = "choices", returnData = TRUE)
 head(choices_mxl_wtp)
-
-# Save results
-saveRDS(choices_mnl_pref, here::here('inst', 'extdata', 'choices_mnl_pref.Rds'))
-saveRDS(choices_mnl_wtp,  here::here('inst', 'extdata', 'choices_mnl_wtp.Rds'))
-saveRDS(choices_mxl_pref, here::here('inst', 'extdata', 'choices_mxl_pref.Rds'))
-saveRDS(choices_mxl_wtp,  here::here('inst', 'extdata', 'choices_mxl_wtp.Rds'))
 
 # Compare prediction accuracy across models
 library(dplyr)
 
+# Add actual choices to every choice prediction data frame
 # Combine models into one data frame
 choices <- rbind(
   choices_mnl_pref, choices_mnl_wtp, choices_mxl_pref, choices_mxl_wtp)
@@ -153,6 +144,7 @@ choices$model <- c(
   rep("mnl_wtp",  nrow(choices_mnl_wtp)),
   rep("mxl_pref", nrow(choices_mxl_pref)),
   rep("mxl_wtp",  nrow(choices_mxl_wtp)))
+
 
 # Compute prediction accuracy by model
 choices %>%
