7d85d4d1f5cfe10a34c95dcdcbd5958959594cdf
John Helveston <john.helveston@gmail.com>
2021-10-16 07:00:42 -0400

    added more test cases for predict

--- tests/testthat/test_predict.R
+++ tests/testthat/test_predict.R
@@ -8,11 +8,50 @@ model <- logitr(
     pars   = c('price', 'feat', 'brand')
 )
 
-alts <- subset(
+data <- subset(
   yogurt, obsID %in% c(42, 13),
   select = c('obsID', 'alt', 'price', 'feat', 'brand'))
 
 test_that("predict() uses model data if newdata == NULL", {
   p <- predict(model)
   expect_equal(nrow(p), nrow(yogurt))
+})
+
+test_that("predict() uses newdata if provided", {
+  p <- predict(model, newdata = data, obsID = "obsID")
+  expect_equal(nrow(p), nrow(data))
+})
+
+test_that("predict() returns the correct user-specified prediction types", {
+  x <- predict(model, newdata = data, obsID = "obsID")
+  expect_true(
+    (! "choice_predict" %in% names(x)) &
+    ("prob_predict" %in% names(x))
+  )
+  x <- predict(model, newdata = data, obsID = "obsID", type = "probs")
+  expect_true(
+    (! "choice_predict" %in% names(x)) &
+    ("prob_predict" %in% names(x))
+  )
+  x <- predict(model, newdata = data, obsID = "obsID", type = "choices")
+  expect_true(
+    ("choice_predict" %in% names(x)) &
+    (! "prob_predict" %in% names(x))
+  )
+  x <- predict(
+    model, newdata = data, obsID = "obsID",
+    type = c("choices", "probs")
+  )
+  expect_true(
+    ("choice_predict" %in% names(x)) &
+    ("prob_predict" %in% names(x))
+  )
+  x <- predict(
+    model, newdata = data, obsID = "obsID",
+    type = c("probs", "choices")
+  )
+  expect_true(
+    ("choice_predict" %in% names(x)) &
+    ("prob_predict" %in% names(x))
+  )
 })
