98ad2d0e5e3bba62361d436687ae9e27f3fc826d
John Helveston <john.helveston@gmail.com>
2022-09-26 13:57:14 -0400

    Update broom.R

--- R/broom.R
+++ R/broom.R
@@ -8,15 +8,19 @@ generics::tidy
 #' @export
 generics::glance
 
-#' `tidy()` broom method for `logitr` class objects.
+
+#' Tidy a `logitr` class object
 #'
 #' @param x is an object of class `logitr`.
-#' @param conf.int Include a confidence interval?
-#' @param level The confidence level required.
-#' @param ... further arguments.
-#'
-#' @return A tibble of the model coefficients and (optional) confidence
-#' intervals.
+#' @param conf.int Logical indicating whether or not to include
+#'   a confidence interval in the tidied output. Defaults to FALSE.
+#' @param conf.level The confidence level to use for the confidence
+#'   interval if conf.int = TRUE. Must be strictly greater than 0
+#'   and less than 1. Defaults to 0.95, which corresponds to a
+#'   95 percent confidence interval.
+#' @param ... Unused, included for generic consistency only.
+#' @return A tidy [tibble::tibble()] summarizing component-level
+#'   information about the model
 #' @export
 #' @examples
 #' library(logitr)
@@ -54,7 +58,7 @@ tidy.logitr <- function(x, conf.int = FALSE, conf.level = 0.95, ...) {
     return(result)
 }
 
-#' `glance()` broom method for `logitr` class objects.
+#' Glance a `logitr` class object
 #'
 #' @param x is an object of class `logitr`.
 #' @param ... further arguments.
